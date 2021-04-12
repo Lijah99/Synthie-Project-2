f4da5ae3303bbb848fb27f2b831a44af8c2317da
thomsondana <dana.thomson@mines.sdsmt.edu>
2021-04-12 11:10:56 -0600

    commit -m "Added tremelo to simulate a one speed leslie speaker. Last thing needed is percussion"

--- Synthie/Organ.cs
+++ Synthie/Organ.cs
@@ -28,6 +28,8 @@ namespace Synthie
             frame = sinewave.Frame();
             // add the drawbar harmonics to the frames
             DrawBars();
+            Vibrato();
+            LeslieTremelo();
             ar.Generate();
 
             // Read the component's sample and make it our resulting frame.
@@ -68,12 +70,30 @@ namespace Synthie
             int[] freqMul = { 1, 3, 2, 4, 6, 8, 10, 12, 16 };
             double[] harmonics = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
             for (int i = 0; i < 9; i++)
-            {
+            { 
                 harmonics[i] = sinewave.Amp * Math.Sin(sinewave.Phase * freqMul[i] * 2 * Math.PI) * (Math.Pow(10, ((draw[i] - 8) * 3) / 20));
                 frame[0] += harmonics[i];
             }
 
             frame[1] = frame[0];
+        }
+
+        // Function to add vibrato to a given frame
+        private void Vibrato()
+        {
+            double vibrato = 0.01;
+            frame[0] += sinewave.Amp * Math.Sin(sinewave.Phase * vibrato * 2 * Math.PI);
+            frame[1] = frame[0];
+
+            vibrato = vibrato * -1;
+        }
+
+        // function to simulate a single speed leslie speaker by adding tremelo
+        private void LeslieTremelo()
+        {
+            float tremelo = (float)(1 + .12 * Math.Sin(4 * 2 * Math.PI * time));
+            frame[0] = frame[0] * tremelo;
+            frame[1] = frame[1] * tremelo;
         }
     }
 }
